054d665deb7d6e6f9cd46814bf5ff24d869b00d9
杨虎 <yanghu@xiaoshouyi.com>
2018-04-17 16:11:55 +0800

    Switch signal

--- MVVM_RACDemo/ViewController.m
+++ MVVM_RACDemo/ViewController.m
@@ -16,20 +16,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    RACSequence *sequence = [@[@"you", @"are", @"beautiful"] rac_sequence];
-    RACSignal *signal =  sequence.signal;
-    RACSignal *capitalizedSignal = [signal map:^id(NSString * value) {
-        return [value capitalizedString];
-        }];
-    [signal subscribeNext:^(NSString * x) {
-        NSLog(@"signal --- %@", x);
-        }];
-    [capitalizedSignal subscribeNext:^(NSString * x) {
-        NSLog(@"capitalizedSignal --- %@", x);
-        }];
+    [self signalSwitch];
 }
 
+- (void)signalSwitch {
+    RACSubject *google = [RACSubject subject];
+    RACSubject *baidu = [RACSubject subject];
+    RACSubject *signalOfSignal = [RACSubject subject];
+    
+    RACSignal *switchSignal = [signalOfSignal switchToLatest];
+    
+    [[switchSignal map:^id _Nullable(id  _Nullable value) {
+        return [NSString stringWithFormat:@"https://www.%@.com",value];
+    }] subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+    
+    [signalOfSignal sendNext:baidu];
+    [baidu sendNext:@"baidu"];
+    [google sendNext:@"google"];
+    NSLog(@"--------seperator line--------");
+    [signalOfSignal sendNext:google];
+    [baidu sendNext:@"baidu"];
+    [google sendNext:@"google"];
+}
 
+- (void)uppercaseString {
+    //    RACSequence *sequence = [@[@"you", @"are", @"beautiful"] rac_sequence];
+    //    RACSignal *signal =  sequence.signal;
+    //    RACSignal *capitalizedSignal = [signal map:^id(NSString * value) {
+    //        return [value capitalizedString];
+    //        }];
+    //    [signal subscribeNext:^(NSString * x) {
+    //        NSLog(@"signal --- %@", x);
+    //        }];
+    //    [capitalizedSignal subscribeNext:^(NSString * x) {
+    //        NSLog(@"capitalizedSignal --- %@", x);
+    //        }];
+    
+    [[[@[@"you", @"are", @"beautiful"] rac_sequence].signal
+      map:^id(NSString * value) {
+          NSLog(@"signal --- %@", value);
+          return [value capitalizedString];
+      }] subscribeNext:^(id x) {
+          NSLog(@"capitalizedSignal --- %@", x);
+      }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
